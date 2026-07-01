4b0fb1771b027a0f9a55c01af1bccdd0a58252fc
Getulio-Higinio <getuliohiginio@hotmail.com>
2026-07-01 14:45:32 +0200

    Update init_db.py

--- olms_app/db/init_db.py
+++ olms_app/db/init_db.py
@@ -1,4 +1,25 @@
-from olms_app.db.database import Base, engine
+from olms_app.db.database import Base, engine, get_session
+from olms_app.db import models
+from olms_app.db.models import User
+from olms_app.auth.login import hash_password
+
 
 def init_db():
     Base.metadata.create_all(bind=engine)
+
+    session = get_session()
+
+    admin = session.query(User).filter(User.username == "admin").first()
+
+    if not admin:
+        admin = User(
+            username="admin",
+            full_name="System Administrator",
+            password_hash=hash_password("admin123"),
+            role="ADMIN",
+            is_active=1,
+        )
+        session.add(admin)
+        session.commit()
+
+    session.close()
